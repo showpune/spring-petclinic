f8871448a42c02661a0bcbf359dabef2c9d841fa
showpune <iamlizhiyong@outlook.com>
2024-07-12 14:48:58 +0800

    update manifest

---
+++ clean.cmd
@@ -0,0 +1,4 @@
+rd /S /Q .azure
+rd /S /Q infra
+del /Q azure.yaml
+rd /S /Q tmp
--- upgrade.cmd
+++
@@ -1,3 +0,0 @@
-mvn clean
-mvn org.openrewrite.maven:rewrite-maven-plugin:run "-Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:RELEASE" "-Drewrite.activeRecipes=org.openrewrite.java.spring.boot3.SpringBoot3BestPractices"
-mvn package -Dmaven.test.skip=true
